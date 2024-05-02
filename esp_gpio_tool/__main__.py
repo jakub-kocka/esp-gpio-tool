2b773290d6f875a667a8f76fdd786e1adcfdac5b
Peter Dragun <peter.dragun@espressif.com>
2024-05-02 12:48:20 +0200

    feat: add argument parser for cli tool

--- esp_gpio_tool/__main__.py
+++ esp_gpio_tool/__main__.py
@@ -1,7 +1,29 @@
-def main() -> str:
-    print('Hello World from ESP GPIO Tool!')
-    return 'Hello World from ESP GPIO Tool!'
+# SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Apache-2.0
+import click
+
+from esp_gpio_tool import __version__
+from esp_gpio_tool.checker import run_check
+
+
+@click.group(no_args_is_help=True)
+@click.version_option(__version__)
+def cli() -> None:
+    pass
+
+
+@cli.command()
+@click.argument('filename', type=click.Path(exists=True), required=True)
+def check(filename: str) -> None:
+    """Check the pin configuration from the given yaml file"""
+    with open(filename, 'r', encoding='UTF-8') as file:
+        user_input = file.read()
+    output = run_check(user_input)
+    for line in output:
+        print(line)
+    if 'All checks passed.' not in output:
+        raise SystemExit(1)
 
 
 if __name__ == '__main__':
-    main()
+    cli()
