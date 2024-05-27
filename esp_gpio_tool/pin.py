24f21bf8170f233d5e119f2614eeb0c4c708ef45
Peter Dragun <peter.dragun@espressif.com>
2024-05-27 15:51:36 +0800

    change: use dataclass for pin

--- esp_gpio_tool/pin.py
+++ esp_gpio_tool/pin.py
@@ -1,34 +1,23 @@
 # SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
 # SPDX-License-Identifier: Apache-2.0
+from dataclasses import dataclass
+from dataclasses import field
 
 
+@dataclass
 class Pin:
+    pin: int
+    power_domain: str
+    is_input: bool = True
+    is_output: bool = True
+    strapping: str | None = None
+    functions: list[str] = field(default_factory=list)
+    reserved: list[str] = field(default_factory=list)
     assigned_function: str | None = None
-    number = 0
-    input = True
-    output = True
-    strapping = None
-    functions: list[str] = []
 
     @property
     def used(self) -> bool:
         return self.assigned_function is not None
-
-    def __init__(
-        self,
-        pin: int,
-        functions: list[str],
-        is_input: bool = True,
-        is_output: bool = True,
-        strapping: str = None,
-        reserved: list[str] = None,
-    ) -> None:
-        self.pin = pin
-        self.functions = functions
-        self.is_input = is_input
-        self.is_output = is_output
-        self.strapping = strapping
-        self.reserved = reserved or []  # reserved functions of the pin for specific variants of chip, e.g. PSRAM
 
     def __str__(self) -> str:
         return f'Pin({self.pin}, {self.functions}), assigned_function={self.assigned_function}'
