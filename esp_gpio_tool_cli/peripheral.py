af9c759ed5359de9c118eaff0389222aa29eaf38
Jakub Kocka <jakub.kocka@espressif.com>
2024-06-25 09:18:51 +0200

    fix(set_mode): Fixed set_mode to be able to work with str and int

--- esp_gpio_tool_cli/peripheral.py
+++ esp_gpio_tool_cli/peripheral.py
@@ -107,6 +107,7 @@ class BasePeripheral:
         if instance not in self.instances:
             raise ValueError(f'Instance {instance} not found. Supported instances: {self.instances}')
         supported_modes = [str(i) for i in self.supported_modes.get(instance, [])]
+        mode = str(mode)
         if mode not in supported_modes:
             raise ValueError(
                 f'Mode {mode} is not supported for {self.name} {instance}. '
