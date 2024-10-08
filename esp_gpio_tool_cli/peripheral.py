71d172790208a419679a98e889da6355bc96901f
Peter Dragun <peter.dragun@espressif.com>
2024-10-08 15:13:57 +0200

    fix: Add missing DTR and DSR signals for UART peripheral

--- esp_gpio_tool_cli/peripheral.py
+++ esp_gpio_tool_cli/peripheral.py
@@ -265,7 +265,7 @@ class UART(BasePeripheral):
         self, count: int, assigned_pins: list[str] = None, universal_pins: list[str] = None, **kwargs: Any
     ) -> None:
         super().__init__(count, assigned_pins, universal_pins, common_prefix=r'U\d.')
-        self.optional_pins = self.unwrap_pins(['U{count}CTS', 'U{count}RTS'])
+        self.optional_pins = self.unwrap_pins(['U{count}CTS', 'U{count}RTS', 'U{count}DTR', 'U{count}DSR'])
         self.reassignable = True
         # TODO print warning if 0 instance is used? probably on PIN side or make UART0 turned on by default?
 
